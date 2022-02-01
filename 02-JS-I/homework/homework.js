791826b9f0dfa31227a9c829c8d853ac71f71958
Nicolas <sotog2106@gmail.com>
2022-02-01 14:28:24 -0500

    homework 1 terminado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -74,9 +74,12 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+ if(str1.length == str2.length) {
+  return true
+ }
+ return false
+}
 
-  }
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
@@ -104,7 +107,7 @@ function mayorQueCincuenta(num) {
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  return x / y
+ return x%y;
 }
 
 
@@ -174,13 +177,12 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-Math.round(numero);
-if(numero%2==0){
-  return "es positivo";
-}else if(numero%2 != 0){
+if(numero>0){
+  return "Es positivo";
+}else if(numero<0){
   return "Es negativo";
 }else{
-  return false
+  return false;
 }
 
 }
@@ -196,25 +198,21 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
- var nombre = "soy";
- var apellido = " henry";
- return nombre + apellido
+ return nombre +" "+ apellido;
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  var nombre = 'martin ';
-var two = 'hola';
-var joined = one + two;
-joined;
-  
+  return "Hola "+ nombre + "!";
+
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+  return alto*ancho;
   
 }
 
@@ -222,6 +220,7 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
+  return lado*4;
   
 }
 
@@ -229,7 +228,7 @@ function retornarPerimetro(lado){
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+return base/2*altura;
 }
 
 
@@ -237,7 +236,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+ return cambio=euro*1.20; 
 }
 
 
@@ -247,7 +246,14 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+ if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
+    return 'Es vocal';
+}
+if (letra.length>1)  {
+    return "Dato incorrecto";
+   }else{
+      return "Dato incorrecto";
+  }
 }
 
 
